34b8a96cb1194602e946f01655e8cb64523a6a87
Andrew Talanow <andrewtalanov@mail.ru>
2021-04-01 12:26:24 +0500

    нормальное меню

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@
 
 window.onload = () => {
 
+    // Burger Menu
+    
+    const btnBurgerMenu = document.querySelector('.btn__brg__menu');
     const burgerMenu = document.querySelector('.burger__menu');
-    const openBurger = document.querySelector("[data-open]");
-    const closeBurger = document.querySelector("[data-close]");
 
-    closeBurger.style.display = "none";
+    // button animation
+    btnBurgerMenu.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    document.querySelector("[data-open]").addEventListener('click', () => {
-        burgerMenu.style.display = "block";
-        openBurger.style.display = "none";
-        closeBurger.style.display = "block";
+        btnBurgerMenu.classList.toggle("is__active");
+
+        if (btnBurgerMenu.matches(".is__active")) {
+            burgerMenu.style.display = "block";
+        } else {
+            burgerMenu.style.display = "none";
+        }
+        
+
     });
 
-    document.querySelector("[data-close]").addEventListener('click', () => {
-        burgerMenu.style.display = "none";
-        closeBurger.style.display = "none";
-        openBurger.style.display = "block";
-    });
+    // -------------
 
     
 
